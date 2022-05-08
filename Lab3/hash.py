6dbf82ad1eed23347ff77d5194e0ffe1324bae2b
“DraymondHIT” <3343033352@qq.com>
2022-05-08 13:10:12 +0800

    完善 Lab3

--- Lab3/hash.py
+++ Lab3/hash.py
@@ -5,7 +5,7 @@ class KHash:
     def __init__(self, k, n):
         self.k = k
         self.n = n
-        self.p = 30031
+        self.p = 436343487
         self.a = random.randint(low=1, high=self.p, size=self.k)
         self.b = random.randint(low=0, high=self.p, size=self.k)
 
--- Lab3/main.py
+++ Lab3/main.py
@@ -1,10 +1,13 @@
 from set import MySet
 from bloomFilter import BloomFilter
 import math
+import random
+import string
 from timeit import default_timer as timer
 
-M = 100
-N = 1000
+epsilon = 1e-2
+M = 29717
+N = int(- M * math.log(epsilon) / math.log(2) / math.log(2))
 K = int(math.log(2, math.e) * N / M)
 
 
@@ -15,38 +18,49 @@ def data():
     return blacklist
 
 
+def test_generator(data):
+    test = random.sample(data, 500)
+    for _ in range(500):
+        len = random.randint(3, 10)
+        random_str = ''.join(random.choice(string.ascii_letters) for _ in range(len))
+        test.append(random_str)
+    return test
+
+
 def insertComparison(myset, bf):
     print('-------------------insert-------------------')
-    names = {'James', 'Jack', 'Sue', 'Luis', 'Kevin', 'Charlotte', 'Leon', 'Neo', 'Destiny', 'Paul'}
+    names = {'james', 'jack', 'sue', 'luis', 'kevin', 'charlotte', 'leon', 'neo', 'destiny', 'paul'}
     tic = timer()
     for name in names:
         myset.insert(name)
     toc = timer()
-    print(f'Set Time: {(toc - tic)*1e6}us')
+    print(f'Set Time: {(toc - tic)*1e6/len(names)}us')
     tic = timer()
     for name in names:
         bf.insert(name)
     toc = timer()
-    print(f'Bloom Filter Time: {(toc - tic)*1e6}us')
+    print(f'Bloom Filter Time: {(toc - tic)*1e6/len(names)}us')
 
 
-def findComparison(myset, bf):
+def findComparison(names, myset, bf):
     print('-------------------find-------------------')
-    names = ['Nancy', 'Ella', 'Carl', 'Ben', 'Julia', 'Winston', 'Leo', 'Jessie', 'Destiny', 'Tommy']
     set_result = []
     bf_result = []
     tic = timer()
     for name in names:
         set_result.append(myset.find(name))
     toc = timer()
-    print(f'Set Time: {(toc - tic)*1e6}us')
+    print(f'Set Time: {(toc - tic)*1e6/len(names)}us')
     tic = timer()
     for name in names:
         bf_result.append(bf.find(name))
     toc = timer()
-    print(f'Bloom Filter Time: {(toc - tic)*1e6}us')
-    print(set_result)
-    print(bf_result)
+    print(f'Bloom Filter Time: {(toc - tic)*1e6/len(names)}us')
+    count = 0
+    for i in range(len(set_result)):
+        if set_result[i] == bf_result[i]:
+            count += 1
+    print(f'Accuracy: {float(count)/len(set_result)}')
 
 
 def main():
@@ -54,7 +68,8 @@ def main():
     myset = MySet(corpus)
     bf = BloomFilter(corpus, K, N)
     insertComparison(myset, bf)
-    findComparison(myset, bf)
+    test = test_generator(corpus)
+    findComparison(test, myset, bf)
 
 
 if __name__ == "__main__":
